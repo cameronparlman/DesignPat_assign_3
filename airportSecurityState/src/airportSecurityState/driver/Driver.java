a4ad924477b9c4823ba8d7fbcd73a30cbf990d66
cameron <cameronparlman@gmail.com>
2017-10-19 00:55:54 -0400

    Basic Structure of program created Driver compiles and runs, test is assumed to run.

---
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -0,0 +1,14 @@
+package airportSecurityState.driver;
+
+
+/** runs the airportSecurityState program 
+@author cameron parlman
+@author cparlma1@binghamton.edu
+@version 0.1
+@since 0.0
+*/
+public class Driver{
+	public static void main(String[] args){
+		System.out.println("Driver is compiling and running TODO(EVERYTHING!!");
+	}
+}
---
+++ airportSecurityState/src/airportSecurityState/test/test.java
@@ -0,0 +1,9 @@
+package airportSecurityState.test;
+
+public class test{
+	public static void main(String[] args){
+		System.out.println("Test Driver for airport Security compile and working");
+
+
+	}
+}
